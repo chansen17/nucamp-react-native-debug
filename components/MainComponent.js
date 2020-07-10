319651fead852b346168765df60c2108b8b31490
Chris Hansen <Chansenwebdev@gmail.com>
2020-07-10 11:12:49 -0700

    Week 1 challenge, contact about components and render completed

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
-        CampsiteInfo: { screen: CampsiteInfo }
+        CampsiteInfo: { screen: CampsiteInfo },
+
     },
     {
         initialRouteName: 'Directory',
@@ -41,10 +44,24 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+const AboutNavigator = createDrawerNavigator(
+    {
+        AboutComponent : { screen: About }
+    }
+);
+
+const ContactNavigator = createDrawerNavigator(
+    {
+        ContactComponent: { screen: Contact }
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator }
     },
     {
         drawerBackgroundColor: '#CEC8FF'
